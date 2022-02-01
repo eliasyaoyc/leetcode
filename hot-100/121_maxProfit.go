29cb2443ba1fd67391e62fc4b8dc7d5894b991ac
Elias.Yao <siran0611@gmail.com>
2022-02-01 20:29:18 +0800

    done.

---
+++ hot-100/121_maxProfit.go
@@ -0,0 +1,15 @@
+package hot_100
+
+func maxProfit(prices []int) int {
+	dp := make([][]int, len(prices))
+	for i := range dp {
+		dp[i] = make([]int, 2)
+	}
+	dp[0][0] = -prices[0]
+	dp[0][1] = 0
+	for i := 1; i < len(prices); i++ {
+		dp[i][0] = max(dp[i-1][0], -prices[i])
+		dp[i][1] = max(dp[i-1][1], dp[i-1][0]+prices[i])
+	}
+	return dp[len(prices)-1][1]
+}
