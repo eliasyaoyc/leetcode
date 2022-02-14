be8bda825aed6e3c2ec35956355ee4381ea4c960
Elias.Yao <siran0611@gmail.com>
2022-02-14 13:56:56 +0800

    done

---
+++ hot-100/142_detectCycle.go
@@ -0,0 +1,31 @@
+package hot_100
+
+// 时间复杂度，空间复杂度为 On
+func detectCycle(head *ListNode) *ListNode {
+	seen := map[*ListNode]struct{}{}
+	for head != nil {
+		if _, ok := seen[head]; ok {
+			return head
+		}
+		seen[head] = struct{}{}
+		head = head.Next
+	}
+	return nil
+}
+
+func detectCycle1(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			p := head
+			for p != slow {
+				p = p.Next
+				slow = slow.Next
+			}
+			return p
+		}
+	}
+	return nil
+}
---
+++ hot-100/148_sortList.go
@@ -0,0 +1,50 @@
+package hot_100
+
+// 自顶向下的归并排序 时间复杂度为 O(nLogN), 空间复杂度为 O(logN)
+func merge1(head1, head2 *ListNode) *ListNode {
+	dummyHead := &ListNode{}
+	temp, temp1, temp2 := dummyHead, head1, head2
+	for temp1 != nil && temp2 != nil {
+		if temp1.Val <= temp2.Val {
+			temp.Next = temp1
+			temp1 = temp1.Next
+		} else {
+			temp.Next = temp2
+			temp2 = temp2.Next
+		}
+		temp = temp.Next
+	}
+	if temp1 != nil {
+		temp.Next = temp1
+	} else if temp2 != nil {
+		temp.Next = temp2
+	}
+	return dummyHead.Next
+}
+
+func sort(head, tail *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	if head.Next == tail {
+		head.Next = nil
+		return head
+	}
+
+	slow, fast := head, head
+	for fast != tail {
+		slow = slow.Next
+		fast = fast.Next
+		if fast != tail {
+			fast = fast.Next
+		}
+	}
+
+	mid := slow
+	return merge1(sort(head, mid), sort(mid, tail))
+}
+
+func sortList(head *ListNode) *ListNode {
+	return sort(head, nil)
+}
